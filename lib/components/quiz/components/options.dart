2e1e707b00cf4a2be1c63110866715f61d830574
marcotuiio <marcotulio-barros@hotmail.com>
2022-04-02 23:59:43 -0300

    pop up está com os botões e toque externo bloqueado, mas não corrige as questões

--- lib/components/quiz/components/options.dart
+++ lib/components/quiz/components/options.dart
@@ -17,103 +17,103 @@ class Option extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<QuestionController>(
-        init: QuestionController(),
-        builder: (qnController) {
-          Color getTheRightColor() {
-            if (qnController.isAnswered) {
-              if (index == qnController.correctAns) {
-                return Colors.green;
-              } else if (index == qnController.selectedAns &&
-                  qnController.selectedAns != qnController.correctAns) {
-                return Colors.red;
-              }
+      init: QuestionController(),
+      builder: (qnController) {
+        Color getTheRightColor() {
+          if (qnController.isAnswered) {
+            if (index == qnController.correctAns) {
+              return Colors.green;
+            } else if (index == qnController.selectedAns &&
+                qnController.selectedAns != qnController.correctAns) {
+              return Colors.red;
             }
-            return Colors.black38;
           }
+          return Colors.black38;
+        }
 
-          IconData getTheRightIcon() {
-            return getTheRightColor() == Colors.red ? Icons.close : Icons.done;
-          }
+        IconData getTheRightIcon() {
+          return getTheRightColor() == Colors.red ? Icons.close : Icons.done;
+        }
 
-          return InkWell(
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => _buildPopupDialog(context),
-              );
-            },
-            child: Container(
-              margin: const EdgeInsets.only(top: 20),
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                border: Border.all(color: getTheRightColor()),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    "${index + 1}. $text",
-                    style: TextStyle(color: getTheRightColor(), fontSize: 16),
-                  ),
-                  Container(
-                    height: 26,
-                    width: 26,
-                    decoration: BoxDecoration(
-                      color: getTheRightColor() == Colors.green
-                          ? Colors.transparent
-                          : getTheRightColor(),
-                      borderRadius: BorderRadius.circular(50),
-                      border: Border.all(color: getTheRightColor()),
-                    ),
-                    child: getTheRightColor() == Colors.grey
-                        ? null
-                        : Icon(getTheRightIcon(), size: 16),
-                  )
-                ],
-              ),
+        return InkWell(
+          onTap: () {
+            showDialog(
+              barrierDismissible: false, //bloqueia toques fora do popup
+              context: context,
+              builder: (BuildContext context) => _buildPopupDialog(context),
+            );
+          },
+          child: Container(
+            margin: const EdgeInsets.only(top: 20),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              border: Border.all(color: getTheRightColor()),
+              borderRadius: BorderRadius.circular(15),
             ),
-          );
-        });
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "${index + 1}. $text",
+                  style: TextStyle(color: getTheRightColor(), fontSize: 16),
+                ),
+                Container(
+                  height: 26,
+                  width: 26,
+                  decoration: BoxDecoration(
+                    color: getTheRightColor() == Colors.green
+                        ? Colors.transparent
+                        : getTheRightColor(),
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(color: getTheRightColor()),
+                  ),
+                  child: getTheRightColor() == Colors.grey
+                      ? null
+                      : Icon(getTheRightIcon(), size: 16),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
 
 Widget _buildPopupDialog(BuildContext context) {
-  final VoidCallback press;
-  
+  //final VoidCallback press;
+
   return AlertDialog(
     title: const Text('Teste pop up questões'),
     content: Column(
       mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: const <Widget>[
-        Text('OLÁ'),
+        Text('TEM CERTEZA?'),
       ],
     ),
     actions: <Widget>[
-      Row(
-        children: <Widget>[
-          ElevatedButton.icon(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.exit_to_app),
-            label: const Text('SAIR'),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.blue[700],
-              onPrimary: Colors.black,
-            ),
-          ),
-          ElevatedButton.icon(
-            onPressed: press,
-            icon: const Icon(Icons.check),
-            label: const Text('CONFIRMAR?'),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.blue[700],
-              onPrimary: Colors.black,
-            ),
-          ),
-        ],
+      ElevatedButton.icon(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: const Icon(Icons.exit_to_app),
+        label: const Text('CANCELAR'),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.red[700],
+          onPrimary: Colors.black,
+        ),
+      ),
+      ElevatedButton.icon(
+        onPressed: () {
+          //press; //aqui deve entrar a verificação da questao certa ou errada
+        },
+        icon: const Icon(Icons.check),
+        label: const Text('CONFIRMAR?'),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.green[700],
+          onPrimary: Colors.black,
+        ),
       ),
     ],
   );
